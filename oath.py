803e4f9c6e74eca301e501d6a6e2df71eb00b411
Sofiya Vasileva <sv6.4@mail.ru>
2026-04-17 23:38:04 +0400

    fixed icon

--- oath.py
+++ oath.py
@@ -1,87 +1,120 @@
 import sys
 import webbrowser
 import requests
-from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
+from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QMessageBox, QStyle
 from PyQt6.QtGui import QIcon
 from PyQt6.QtNetwork import QTcpServer, QHostAddress
 
-# Конфигурация приложения Яндекс
 CLIENT_ID = '4648a51ecff4419999228cdb14a168c4'
-CLIENT_SECRET = '4648a51ecff4419999228cdb14a168c4'
+CLIENT_SECRET = '249440f3331c493083ad045e1f92f814'
+REDIRECT_PORT = 8080
 
-class CloudAuthManager(QTcpServer):
-    def __init__(self):
-        super().__init__()
+class AuthServer(QTcpServer):
+    """Сервер для автоматического приема кода авторизации"""
+    def __init__(self, parent=None):
+        super().__init__(parent)
         self.access_token = None
         
-        self.listen(QHostAddress.SpecialAddress.LocalHost, 8080)
+        if not self.listen(QHostAddress.LocalHost, REDIRECT_PORT):
+            print(f"Не удалось запустить сервер на порту {REDIRECT_PORT}")
+        
         self.newConnection.connect(self.handle_connection)
 
     def handle_connection(self):
         client = self.nextPendingConnection()
         if client:
-            # Читаем данные из запроса браузера
             while client.canReadLine():
                 line = client.readLine().data().decode()
+                # Ищем GET запрос, который пришлет Яндекс после клика пользователя
                 if "GET" in line:
                     try:
+                       
                         code = line.split("code=")[1].split(" ")[0]
                         self.exchange_code_for_token(code)
                         
-                        response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"
-                        response += "<h1>Успешно!</h1><p>Облако подключено. Можно закрыть вкладку.</p>"
+                        response = (
+                            "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"
+                            "<html><body style='font-family: sans-serif; text-align: center; margin-top: 50px;'>"
+                            "<h2 style='color: #2c3e50;'>Авторизация успешна!</h2>"
+                            "<p>Приложение получило ключ. Теперь вы можете закрыть эту вкладку.</p>"
+                            "</body></html>"
+                        )
                         client.write(response.encode())
-                    except IndexError:
-                        pass
+                    except Exception as e:
+                        print(f"Ошибка при разборе кода: {e}")
             client.disconnectFromHost()
 
     def exchange_code_for_token(self, code):
+        """Обмен кода на токен через API Яндекса"""
+        url = "https://oauth.yandex.ru/token"
         data = {
             'grant_type': 'authorization_code',
             'code': code,
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET
         }
-        r = requests.post("https://oauth.yandex.ru/token", data=data)
-        self.access_token = r.json().get('access_token')
-        print(f"Токен получен: {self.access_token}")
-        
-        from PyQt6.QtWidgets import QMessageBox
-
-        def exchange_code_for_token(self, code):
+        try:
+            r = requests.post(url, data=data)
+            result = r.json()
+            self.access_token = result.get('access_token')
             
             if self.access_token:
-                msg = QMessageBox()
-                msg.setText(f"Успех! Мы получили доступ.\nТокен: {self.access_token[:10]}...")
-                msg.exec()
-        
+                print(f"\n[УСПЕХ] Токен получен: {self.access_token}")
+                self.on_success()
+            else:
+                print(f"[ОШИБКА] Яндекс не выдал токен: {result}")
+        except Exception as e:
+            print(f"[ОШИБКА] Проблема с сетью: {e}")
 
-class TrayApp:
+    def on_success(self):
+        """Действия после успешного получения ключа"""
+        msg = QMessageBox()
+        msg.setWindowTitle("Яндекс.Диск")
+        msg.setText("Авторизация прошла успешно!\nТеперь облако будет подключено к вашей папке Home/cloud.")
+        msg.exec()
+        # Здесь ваша команда вставит функцию монтирования:
+        # mount_folder(self.access_token)
+
+class CloudTrayApp:
     def __init__(self):
-        self.app = QApplication(sys.argv)
-        self.auth_manager = CloudAuthManager()
+        # Создаем приложение Qt
+        self.qt_app = QApplication(sys.argv)
+        self.qt_app.setQuitOnLastWindowClosed(False) # Чтобы приложение не закрылось без окон
+
+        # Запускаем локальный сервер
+        self.server = AuthServer()
+
+        # Настраиваем иконку (берем стандартную "Drive" из системы)
+        icon = self.qt_app.style().standardIcon(QStyle.StandardPixmap.SP_DriveHDIcon)
         
-        # Настройка трея
-        self.tray = QSystemTrayIcon(QIcon.fromTheme("drive-removable-media"))
-        menu = QMenu()
+        # Создаем трей
+        self.tray = QSystemTrayIcon(icon)
+        self.tray.setToolTip("Яндекс.Диск Альт Линукс")
+
+        # Создаем меню трея
+        self.menu = QMenu()
         
-        login_btn = menu.addAction("Войти в Яндекс.Диск")
-        login_btn.triggered.connect(self.open_browser)
+        login_action = self.menu.addAction("Войти и подключить Диск")
+        login_action.triggered.connect(self.open_browser)
         
-        menu.addSeparator()
-        exit_btn = menu.addAction("Выход")
-        exit_btn.triggered.connect(sys.exit)
+        self.menu.addSeparator()
         
-        self.tray.setContextMenu(menu)
+        exit_action = self.menu.addAction("Выйти из программы")
+        exit_action.triggered.connect(sys.exit)
+
+        self.tray.setContextMenu(self.menu)
         self.tray.show()
 
     def open_browser(self):
+        """Открывает браузер для авторизации"""
         url = f"https://oauth.yandex.ru/authorize?response_type=code&client_id={CLIENT_ID}"
+        print(f"Открываем ссылку: {url}")
         webbrowser.open(url)
 
     def run(self):
-        sys.exit(self.app.exec())
+        # Запуск цикла событий Qt
+        return self.qt_app.exec()
 
 if __name__ == "__main__":
-    tray_app = TrayApp()
-    tray_app.run()
+    app = CloudTrayApp()
+    sys.exit(app.run())
